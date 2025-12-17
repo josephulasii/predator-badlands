80f209399c10c8cb5e55437d122f506ea24e1307
josephulasii <jojoulasi7@gmail.com>
2025-12-17 20:22:18 +0000

    Add main.py to run simulation

---
+++ src/main.py
@@ -0,0 +1,26 @@
+
+from simulation import Simulation
+
+
+def main():
+    simulation = Simulation()
+
+    while simulation.turncount < 20:
+        simulation.run_turn()
+        simulation.display()
+        
+        if simulation.dek.is_alive() == False:
+            print("Dek has been killed")
+            break
+
+        if simulation.adversary.is_alive() == False:
+            print("Dek has Killed The Boss")
+            break
+
+
+    
+        
+
+
+if __name__ == "__main__":
+    main()
